6661f737b2b0adb472f0e9070013c0cf1c1033fe
kuboryu <79005540+kbtrg@users.noreply.github.com>
2021-12-08 07:18:29 +0900

    完了ボタンメソッド上手くいかない... js-done-btnが取得できていない?

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@
 const todoValue = document.getElementById("js-todo-ttl"); //入力情報取得
 const todoRegister = document.getElementById("js-register-btn"); //「登録する」ボタン
 const todoList = document.getElementById("js-todo-list"); //ulタグ取得
-
-const doneBtn = document.getElementById("js-done-btn"); //完了ボタン取得
-const delBtn = document.getElementById("js-del-btn"); //削除ボタン取得
+const doneList = document.getElementById("js-done-list"); //ulタグ取得
 
 /*「登録する」ボタンのイベント*/
 todoRegister.addEventListener("click", () => {
@@ -15,14 +13,14 @@ todoRegister.addEventListener("click", () => {
   const btns = document.createElement("div"); //btnレイアウトを作る準備
   btns.setAttribute("class", "buttons"); //btnのレイアウトを作る為のクラス付与
   //完了ボタン
-  const donebtn = document.createElement("button");
+  const donebtn = document.createElement("button"); //buttonタグ作成
   donebtn.setAttribute("id", "js-done-btn"); //idを付与
   donebtn.innerHTML = "完了"; //buttonタグの中身文字
+  console.log(document.getElementById("js-done-btn"));
   //削除ボタン
   const delbtn = document.createElement("button");
   delbtn.setAttribute("id", "js-del-btn");
   delbtn.innerHTML = "削除";
-
   //ul>li>p構造を作る
   ptag.appendChild(todo); //pタグの子要素に入力値を追加
   litag.appendChild(ptag); //liタグの子要素にpタグを追加
@@ -33,3 +31,11 @@ todoRegister.addEventListener("click", () => {
 });
 
 /*完了ボタンのイベント*/
+if (document.getElementById("js-done-btn") !== null) {
+  console.log(document.getElementById("js-done-btn"));
+  const doneBtn = document.getElementById("js-done-btn"); //完了ボタン取得
+  console.log(doneBtn);
+  doneBtn.addEventListener("click", () => {
+    doneBtn.closest("li").appendTo(doneList); //完了ボタンの親のliタグをdoneListに挿入
+  });
+}
