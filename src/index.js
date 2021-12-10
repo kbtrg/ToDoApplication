ed498f7ca038823efc0f50ed05630390aba1101a
kuboryu <79005540+kbtrg@users.noreply.github.com>
2021-12-10 09:24:02 +0900

    大体完成！ 登録・完了・戻す・削除機能は実装完了。あとは、0文字タスクの登録不可機能あたり。

--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
-/*getElement*/
-const todoValue = document.getElementById("js-todo-ttl"); //入力情報取得
-const todoRegister = document.getElementById("js-register-btn"); //「登録する」ボタン
+const taskValue = document.getElementById("js-todo-ttl"); //入力情報取得
+const taskAddBtn = document.getElementById("js-register-btn"); //「登録する」ボタン
 const todoList = document.getElementById("js-todo-list"); //ulタグ取得
 const doneList = document.getElementById("js-done-list"); //ulタグ取得
 
-/*「登録する」ボタンのイベント*/
-todoRegister.addEventListener("click", () => {
-  const todo = document.createTextNode(todoValue.value); //入力値取得
+taskAddBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const task = taskValue.value;
+  addTask(task);
+  taskValue.value = ""; //入力値の初期化
+});
+
+//登録メソッド
+const addTask = (task) => {
+  const todo = document.createTextNode(task); //入力値取得
   const litag = document.createElement("li"); //liタグを作る準備
   litag.className = "list"; //liタグにクラス名付与
   const ptag = document.createElement("p"); //pタグを作る準備
   const btns = document.createElement("div"); //btnレイアウトを作る準備
   btns.setAttribute("class", "buttons"); //btnのレイアウトを作る為のクラス付与
-  //完了ボタン
+  /*完了ボタン*/
   const donebtn = document.createElement("button"); //buttonタグ作成
-  donebtn.setAttribute("id", "js-done-btn"); //idを付与
+  donebtn.setAttribute("class", "js-done-btn"); //classを付与
   donebtn.innerHTML = "完了"; //buttonタグの中身文字
-  console.log(document.getElementById("js-done-btn"));
-  //削除ボタン
+  //完了ボタンのイベント
+  donebtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    doneTask(donebtn);
+  });
+  /*削除ボタン*/
   const delbtn = document.createElement("button");
-  delbtn.setAttribute("id", "js-del-btn");
+  delbtn.setAttribute("class", "js-del-btn");
   delbtn.innerHTML = "削除";
-  //ul>li>p構造を作る
+  //削除ボタンのイベント
+  delbtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    deleteTask(delbtn);
+  });
+  //登録後の要素の追加(ul>li>p構造を作る)
   ptag.appendChild(todo); //pタグの子要素に入力値を追加
   litag.appendChild(ptag); //liタグの子要素にpタグを追加
   btns.appendChild(donebtn); //btnsの子要素にdonebtnを追加
   btns.appendChild(delbtn); //btnsの子要素にdelbtnを追加
   litag.appendChild(btns); //liタグの子要素にbtnsクラスを持つdivタグを追加
   todoList.appendChild(litag); //ulタグの子要素にliタグを追加
-});
-
-/*完了ボタンのイベント*/
-if (document.getElementById("js-done-btn") !== null) {
-  console.log(document.getElementById("js-done-btn"));
-  const doneBtn = document.getElementById("js-done-btn"); //完了ボタン取得
-  console.log(doneBtn);
-  doneBtn.addEventListener("click", () => {
-    doneBtn.closest("li").appendTo(doneList); //完了ボタンの親のliタグをdoneListに挿入
+};
+/*完了ボタンメソッド*/
+const doneTask = (btn) => {
+  btn.innerHTML = "戻す"; //完了ボタンを戻すボタンに変更
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    returnTask(btn);
   });
-}
+  const chosenTask = btn.closest("li"); //移動するリストを選択
+  doneList.appendChild(chosenTask); //Doneリストに移動
+};
+/*戻すボタンメソッド*/
+const returnTask = (btn) => {
+  btn.innerHTML = "完了"; //戻すボタンを完了ボタンに変更
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    doneTask(btn);
+  });
+  const chosenTask = btn.closest("li"); //移動するリストを選択
+  todoList.appendChild(chosenTask); //Doneリストに移動
+};
+/*削除ボタンメソッド*/
+const deleteTask = (delbtn) => {
+  const chosenTask = delbtn.closest("li"); //削除するリストを選択
+  chosenTask.remove();
+};
